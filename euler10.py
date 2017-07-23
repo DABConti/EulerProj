e7f789b25a75a3ee40f8e689cfbbb53d3fd725ec
Daniel Conti <hoshi1990@gmail.com>
2017-07-23 14:28:57 -0700

    Finished working problems 10 and 11

--- euler10.py
+++ euler10.py
@@ -1,19 +1,39 @@
 #!/usr/bin/python
+import math
+
 def main():
+	"""
+	The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+	Find the sum of all the primes below two million.
+	"""
+	#some primes to start out
 	primes=[]
-	x=15
-	i=0
-	while primes[len(primes)-1]<100000:
-		while i< len(primes):
-			if x%primes[i]==0:
-				break;
-			i+=1
-			if i==len(primes):
-				primes.append(x)
-		i=0
-		x+=2
-	print primes[len(primes)-1]
+	cutoff = 2000000
 	
+	i=0
+	
+	#create slieve of erastosthenes
+	slieve = range(2, cutoff)
+	
+	#print slieve
+	
+	#square root index
+	sqrtCutoff = math.sqrt(cutoff) 
+	while i < sqrtCutoff:
+		j = i
+		if(slieve[i] != -1):
+			
+			while j + slieve[i] < len(slieve):
+				j = j + slieve[i]
+				#print i, slieve[i], j
+				slieve[j] = -1
+			
+		i+=1
+	
+	primes = [x for x in slieve if x != -1]
+	print primes
+	print sum(primes)
 
 if __name__ == "__main__":
     main()
